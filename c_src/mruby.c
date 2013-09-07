0253b461fd6704e1eb954276c3782125cd1690d2
Yuki Ito <yuki@gnnk.net>
2013-09-07 21:49:40 +0900

    mruby2erl

--- c_src/mruby.c
+++ c_src/mruby.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 #include <mruby.h>
 #include <mruby/proc.h>
 #include <mruby/compile.h>
@@ -6,6 +8,21 @@
 
 static int _mrb_fixnum(mrb_value o) { return (int) mrb_fixnum(o); }
 static float _mrb_float(mrb_value o) { return (float) mrb_float(o); }
+
+static ERL_NIF_TERM mruby2erl(ErlNifEnv* env, mrb_value value) {
+  switch(value.tt) {
+    case MRB_TT_TRUE:
+      return enif_make_atom(env, "true");
+    case MRB_TT_FALSE:
+      return enif_make_atom(env, "false");
+    case MRB_TT_FIXNUM:
+      return enif_make_int(env, _mrb_fixnum(value));
+    case MRB_TT_FLOAT:
+      return enif_make_double(env, _mrb_float(value));
+    default :
+      return enif_make_string(env, "undefined return type", ERL_NIF_LATIN1);
+  }
+}
 
 static ERL_NIF_TERM eval(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
   ErlNifBinary script_binary;
@@ -25,27 +42,22 @@ static ERL_NIF_TERM eval(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
 
   cxt = mrbc_context_new(mrb);
 
-  mrb_value result = mrb_load_string_cxt(mrb, (const char *)script_binary.data, cxt);
+  char *script = malloc(script_binary.size);
+
+  strncpy(script, (const char *)script_binary.data, (int)script_binary.size);
+
+  mrb_value result = mrb_load_string_cxt(mrb, (const char *)script, cxt);
+
+  free(script);
 
   mrbc_context_free(mrb, cxt);
   mrb_close(mrb);
 
   enif_release_binary(&script_binary);
 
-  switch(result.tt) {
-    case MRB_TT_TRUE:
-      return enif_make_atom(env, "true");
-    case MRB_TT_FALSE:
-      return enif_make_atom(env, "false");
-    case MRB_TT_FIXNUM:
-      return enif_make_int(env, _mrb_fixnum(result));
-    case MRB_TT_FLOAT:
-      return enif_make_double(env, _mrb_float(result));
-    default :
-      return enif_make_string(env, "undefined return type", ERL_NIF_LATIN1);
-  }
+  ERL_NIF_TERM erl_result = mruby2erl(env, result);
 
-  return enif_make_atom(env, "ok");
+  return erl_result;
 }
 
 static ErlNifFunc nif_funcs[] =
